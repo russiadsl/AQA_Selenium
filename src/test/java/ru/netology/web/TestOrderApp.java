ddd9f795f3bd0f3241d37cd2aef79fbbe9473aff
russiadsl <bezugly_spb@mail.ru>
2022-05-09 17:18:46 +0300

    Added new webdriver for linux

--- src/test/java/ru/netology/web/TestOrderApp.java
+++ src/test/java/ru/netology/web/TestOrderApp.java
@@ -20,7 +20,7 @@ public class TestOrderApp {
 
     @BeforeAll
     public static void setUp(){
-        System.setProperty("webdriver.chrome.driver" , "driver/chromedriver");
+        System.setProperty("webdriver.chrome.driver" , "driver/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
